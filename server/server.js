6fdae361b6119d0c7673d56cb117716dddcc8711
Yashwanth Durseti <yashwanthdurseti@gmail.com>
2024-10-29 22:05:07 +0530

    package file updation four

--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path'); // Import path module
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('public')); // Serve static files from the public directory
 
 let classes = [], bookings = [];
 
 app.post('/api/classes', (req, res) => {
     classes = [...classes, ...req.body];
     res.status(201).json({ message: 'Classes created', classes });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Serve index.html
 });
 
 app.get('/api/classes', (req, res) => {
@@ -29,5 +35,5 @@ app.get('/api/bookings', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
